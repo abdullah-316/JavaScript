172783d5b5d54028179baf2a6b877ebb6a0f95d7
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-05 00:20:04 +0500

    first commit

---
+++ 1-Basics/Basics.js
@@ -0,0 +1,8 @@
+
+const accountId = 12233;
+let accountHolder = "Roman";
+let accountEmail = "OTC@gmail.com";
+let city = "New York";
+
+// console.log(accountId);
+console.table([accountId, accountHolder, accountEmail, city])
