6f282732232c29979d4de426121f93d3b9c08b26
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-05 00:21:36 +0500

    first commit

---
+++ 2-Operators/Operators.js
@@ -0,0 +1,21 @@
+
+
+let add = 2 + 2
+let sub = 3 - 2
+let mul = 3 * 5 
+let div =  4 / 2
+let pow = 2 ** 3
+let module =  3 % 2
+
+// console.table([add, sub, mul, div, pow, module]);
+
+// Post increment
+let a = 100;
+let b = a++;
+console.log(`a:${a}, b:${b}`);
+
+// Pre increment
+let x = 100;
+let y = ++x;
+console.log(`x:${x}, y:${y}`);
+
