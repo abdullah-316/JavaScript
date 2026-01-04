89dc8508dd651ef816f19b20b62d5e5e56b548f4
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-05 00:25:41 +0500

    first commit

--- 3-Comparison/Comparison.js
+++ 3-Comparison/Comparison.js
@@ -3,21 +3,25 @@
 let n1 = 5;
 let n2 = 3
 
+// we check for n1
 
-
-// console.log(n1 > n2);
-// console.log(n1 < n2);
-
-// console.log(n1 >= n2);
-// console.log(n1 <= n2);
-// console.log(n1 != n2);
-
-// console.log(n1 == n2);
+// Less Then
+console.log(n1 > n2);
+// Greater Then
+console.log(n1 < n2);
+// Less Then Equal to
+console.log(n1 >= n2);
+// Greater Then Equal to
+console.log(n1 <= n2);
+// Not Equal to
+console.log(n1 != n2);
+// Equal Equal to
+console.log(n1 == n2);
 
 // NULL EXAMPLE
-// console.log(null == 0);
-// console.log(null > 0);
-// console.log(null >= 0);
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
 
 /*  The Reason is that an equality (==) check
 and Comparison (<,>,<=,>=) work differently. 
