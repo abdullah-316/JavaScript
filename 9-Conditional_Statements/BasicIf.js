a79c93241ade36958677424f2d8b78a9b9ffaddc
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-14 22:58:45 +0500

    first commit

---
+++ 9-Conditional_Statements/BasicIf.js
@@ -0,0 +1,17 @@
+
+
+// 3. Traffic light system Example:
+let tarfficLight = "Green"
+
+if (tarfficLight === "Red") {
+    console.log("Slow Down");
+    
+}
+if (tarfficLight === "Yellow") {
+    console.log("Ready");
+    
+}
+if (tarfficLight === "Green") {
+    console.log("Go");
+    
+}
