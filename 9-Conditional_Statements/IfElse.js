5e44bb37f89edcbc7b9740736ff73165b89d6fb8
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-14 22:59:01 +0500

    first commit

---
+++ 9-Conditional_Statements/IfElse.js
@@ -0,0 +1,17 @@
+
+
+// 4. Popcorn prices Example:
+let size = 'xl';
+
+if(size === 'xl'){
+    console.log("Your popcorn price is 250 rupee");
+}
+else if(size === 'l'){
+    console.log("Your popcorn price is 200 rupee");
+}
+else if(size === 'm'){
+    console.log("Your popcorn price is 100 rupee");
+}
+else{
+    console.log("Your popcorn price is 50 rupee");
+}
