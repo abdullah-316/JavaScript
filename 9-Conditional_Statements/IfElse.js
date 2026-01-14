af0c25b7877162b8b2720146a06b1088af95fbae
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-14 23:06:56 +0500

    first commit

--- 9-Conditional_Statements/IfElse.js
+++ 9-Conditional_Statements/IfElse.js
@@ -15,3 +15,16 @@ else if(size === 'm'){
 else{
     console.log("Your popcorn price is 50 rupee");
 }
+
+
+// Question: Create a number variable num with some value. Now, print “good” if the number is divisible by 10 and print “bad” if it is not.
+
+let num = 50;
+
+if(num % 10 == 0){
+    console.log("Good");
+    
+}
+else{
+    console.log("Bad");
+}
