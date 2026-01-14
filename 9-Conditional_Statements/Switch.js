668b4a8e5982cd8db8efaf4f5135dad893d1a38a
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-14 22:59:41 +0500

    first commit

---
+++ 9-Conditional_Statements/Switch.js
@@ -0,0 +1,20 @@
+
+let months = 2;
+
+switch (months) {
+    case 1:
+        console.log("January, February, March");
+        break;
+    case 2:
+        console.log("April, May, June");
+        break;
+    case 3:
+        console.log("July, August, September");
+        break;
+    case 4:
+        console.log("October, November, December");
+        break;
+    default:
+        console.log("Invalid Number!");
+        break;
+}
