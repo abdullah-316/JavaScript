4ac57f90bbd817d20c35e8d49f18d16b16771151
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-14 22:59:27 +0500

    first commit

---
+++ 9-Conditional_Statements/StringCheck.js
@@ -0,0 +1,21 @@
+
+
+let str = 'amma';
+
+if (str[0] === 'a' && str.length > 3) {
+    console.log("Good String");
+}
+else{
+    console.log("Not a Good String");
+    
+}
+
+
+let character = "Aesthetic";
+
+if ((character[0] === 'A' || 'a' ) &&  character.length > 5 ) {
+    console.log("Golden String"); 
+}
+else{
+    console.log("Not a Golden String");
+}
