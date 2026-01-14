3e6a16a9af7881cedc029124e0d4736ed32dd8b7
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-14 23:06:45 +0500

    first commit

--- 9-Conditional_Statements/StringCheck.js
+++ 9-Conditional_Statements/StringCheck.js
@@ -10,6 +10,7 @@ else{
     
 }
 
+// Question: A string is a golden string if it starts with the character ‘A’ or ‘a’ and has a total length greater than 5. For a given string, print if it is golden or not.
 
 let character = "Aesthetic";
 
