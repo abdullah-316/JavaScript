b7c6b906fee869eacf0e08b9e248faa588fcb6b1
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-15 17:18:40 +0500

    first commit

---
+++ 10-Loops/app.js
@@ -0,0 +1,91 @@
+
+
+
+// print Odd Numbers
+// console.log("Odd Numbers");
+
+// let n = 15;
+// for(let i=1; i<=n; i++ ){
+//     if(i % 2 == 1){
+//         console.log(i);  
+//     }
+// }
+
+
+// // Print Even Numbers
+// console.log("Even Numbers");
+// // let num = 15;
+// // for(let j=1; j<n; j++){
+// //     if(j % 2 == 0){
+// //         console.log(j);
+        
+// //     }
+// // }
+// let num = 10
+// for(let j=2; j<=num; j=j+2){
+//     console.log(j);
+// }
+
+
+
+
+// // Print Multipication Table:
+// let n = 5;
+// for(let i=1; i<=10; i++){
+//     console.log(`${n} x ${i} = ${n * i}`);
+// }
+
+
+
+// // Guess the movie name game:
+// const favSports = "cricket";
+// let guess = prompt("Guess the favourite sport");
+
+// while((guess != favSports) && (guess != 'quit')){
+//    guess = prompt("Wrong answer! please try again.")
+// }
+
+// if(guess == favSports){
+//     console.log("Congrats");
+// }
+// else{
+//     console.log("you quit")
+// }
+
+
+
+
+// // Loops using Arrays:
+// let fruits = ["mango", "apple", "banana", "strawberry", "peach"]
+
+// for(let i=0; i<=fruits.length-1; i++){
+//     console.log(i, fruits[i]);
+// }
+
+
+// Nested Arrays using Loops:
+// let student = [
+//     ["Abdullah", 98], ["Osman", 97.5], ["Fahad", 100]
+// ];
+
+// for(let i=0; i<student.length; i++){
+//     console.log(`info of Student #${i+1}`);
+    
+//     for(let j=0; j<student[i].length; j++){
+//         console.log(student[i][j]);
+//     }
+// }
+
+
+// // For of Loops Example
+// let students = [
+//     ["Abdullah", 98], ["Osman", 97.5], ["Fahad", 100]
+// ];
+
+// for(student of students){
+//     console.log(`info of Students:`);
+
+//     for(list of student){
+//         console.log(list);
+//     }
+// }
