56f6fdbc5753a877dc58dc39801f53b6fb66424a
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-15 17:51:23 +0500

    first commit

--- 10-Loops/practice.js
+++ 10-Loops/practice.js
@@ -1,54 +1,55 @@
 // Qs1. Write a JS program to delete all occurrences of element ‘num’ in a given array.
 // Example : if arr = [1, 2, 3, 4, 5, 6, 2, 3] & num = 2
 
-// let arr = [1, 2, 3, 4, 5, 6, 2, 3];
+const arr = [1, 2, 3, 4, 5, 6, 2, 3];
+let mynum = 2
 
-// for(let i=1; i<arr.length; i++){
-//   if(arr[i] === 2){
-//     arr.splice(i, 1);
-//   }
-// }
-// console.log(arr); // Result should be arr = [1, 3, 4, 5, 6, 3]
+for(let i=1; i<arr.length; i++){
+  if(arr[i] === mynum){
+    arr.splice(i, 1);
+  }
+}
+console.log(arr); // Result should be arr = [1, 3, 4, 5, 6, 3]
 
 
 
 // // Qs2. Write a JS program to find the no of digits in a number.
-// // Example : if number = 287152, count = 6
-// let num = 287152;
-// count = 0;
-// while (num > 0) {
-//     count++;
-//     num = Math.floor(num / 10);  
-// }
-// console.log(count);
+// // Example : if number = 287152, so my count = 6
+let num = 287152;
+count = 0;
+while (num > 0) {
+    count++;
+    num = Math.floor(num / 10);  
+}
+console.log(count);
 
 
 
 // // Qs3. Write a JS program to find the sum of digits in a number.
-// // Example : if number = 287152, sum = 25
-// let num = 287152;
-// sum = 0;
-// while (num > 0) {
-//     let digit = num % 10;
-//     sum = sum + digit;
-//     num = Math.floor(num / 10);  
-// }
-// console.log(sum);
+// // Example : if number = 287152, so my sum = 25
+let n = 287152;
+sum = 0;
+while (n > 0) {
+    let digit = n % 10;
+    sum = sum + digit;
+    n = Math.floor(n / 10);  
+}
+console.log(sum);
 
 
 
 // // Qs4. Print the factorial of a number n.
-// let fact = 1;
-// let num = 4;
+let fact = 1;
+let number = 4;
 
-// for(let i=1; i<=num; i++){
-//   fact = fact * i;
-// }
-// console.log(`Factorial of ${num} is ${fact}.`);
+for(let i=1; i<=number; i++){
+  fact = fact * i;
+}
+console.log(`Factorial of ${number} is ${fact}.`);
 
 
 // // Qs5. Find the largest number in an array with only positive numbers
-let arr = [2, 4, 5, 8, 1];
+let myArr = [2, 4, 5, 8, 1];
 let largest = 0;
 
 for(let i=1; i<arr.length; i++){
