bfa5859ac756664f82a71288d0f911888c2cafcc
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-15 17:51:52 +0500

    first commit

---
+++ 10-Loops/ForLoops.js
@@ -0,0 +1,35 @@
+
+let num = 10
+for(let i=1; i<=num; i++){
+    console.log(i);
+}
+
+
+
+// // print Odd Numbers
+console.log("Odd Numbers");
+
+let n = 15;
+for(let i=1; i<=n; i++ ){
+    if(i % 2 == 1){
+        console.log(i);  
+    }
+}
+
+
+// Print Even Numbers
+console.log("Even Numbers");
+let number = 15;
+for(let j=1; j<number; j++){
+    if(j % 2 == 0){
+        console.log(j);
+        
+    }
+}
+
+
+//  Print Multipication Table:
+let value = 5;
+for(let i=1; i<=10; i++){
+    console.log(`${value} x ${i} = ${value * i}`);
+}
