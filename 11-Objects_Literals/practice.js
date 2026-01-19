f8a36d79646db96bfc3814271336ea15b85e4662
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-19 23:50:10 +0500

    first commit

---
+++ 11-Objects_Literals/practice.js
@@ -0,0 +1,27 @@
+// Qs1. Create a program that generates a random number representing a dice roll.
+// [The number should be between 1 and 6].
+
+const random = Math.floor(Math.random() * 6) + 1;
+
+
+
+// Qs2. Create an object representing a car that stores the following properties for the
+// car: name, model, color.
+// Print the car’s name
+
+let car = {
+    name: 'Honda',
+    model: 2025,
+    color: ['grey', 'white', 'black']
+};
+
+
+// Qs3. Create an object Person with their name, age and city.
+// Edit their city’s original value to change it to “New York”.
+// Add a new property country and set it to the United States.
+
+let person = {
+    name: 'Abdullah',
+    age: 24,
+    city: 'Lahore'
+};
