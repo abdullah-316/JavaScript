864e39f5cff454c322a891376ffebd9ff9151c86
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-19 23:48:10 +0500

    first commit

---
+++ 11-Objects_Literals/app.js
@@ -0,0 +1,71 @@
+// // Object Literals Syntax
+// const student = {   // student is creating as an Object or Property.
+//     name: 'Abdullah', // name = Key and 'Abdullah' = value
+//     age: 24,
+//     marks: 96.5
+// };
+
+// // We can also create an array in object.
+// const items = {
+//     price: 99.99,
+//     discount: 5,
+//     color: ['red', 'green']
+// };
+
+
+// // Create a X post:
+// const post = {
+//     username: "@elon.musk",
+//     content: "NASA and Tesla",
+//     likes: 1000,
+//     repost: 100,
+//     tags: ['@nasa', '@tesla']
+// };
+
+
+
+// Nested Objects (Objects of Objects)
+const classInfo = {
+    std1: {
+        name: 'Ali',
+        age: 22,
+        marks: 90
+    },
+    std2: {
+        name: 'Hamza',
+        age: 25,
+        marks: 94
+    },
+    std3: {
+        name: 'Usman',
+        age: 23,
+        marks: 92
+    },
+};
+
+
+
+// Array of Objects:
+const studentInfo = [
+    {
+        name: 'Ali',
+        age: 22,
+        marks: 90
+    },
+
+    {
+        name: 'Usman',
+        age: 23,
+        marks: 92
+    },
+
+    {
+        name: 'Hamza',
+        age: 25,
+        marks: 94
+    },
+];
+
+
+// Random Integer:
+let random = Math.floor( Math.random() * 10 ) + 1;
