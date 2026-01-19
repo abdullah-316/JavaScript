f1da33623ed1ac75c1635e707f50e14578e0721b
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-19 23:53:57 +0500

    first commit

---
+++ 11-Objects_Literals/ArrayObjects.js
@@ -0,0 +1,20 @@
+// Array of Objects:
+const studentInfo = [
+    {
+        name: 'Ali',
+        age: 22,
+        marks: 90
+    },
+
+    {
+        name: 'Usman',
+        age: 23,
+        marks: 92
+    },
+
+    {
+        name: 'Hamza',
+        age: 25,
+        marks: 94
+    },
+];
