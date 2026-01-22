b03e4d9a8558b73a75e34f8edc7518e569efd4ea
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-22 07:30:06 +0500

    first commit

--- 12-Functions/IIFE.js
+++ 12-Functions/IIFE.js
@@ -1,19 +1,16 @@
-
-
 // Immediately Invoked Function Expressions (IIFE)
 // IIFE means: It is a function expression that executes immediately after it is defined. Keeps variables private (no pollution of global scope)
 
 // Syntax:
 // 1- basic and named with parameter IIFE
 (function myFunc(name) {
-    console.log(`DB connected ${name}`);
+  console.log(`DB connected ${name}`);
 })("Successfully");
-
 
 // Using with Arrow Function
 // Un-named without parameter IIFE
-( () => {
-    console.log("Connection Established");
-})()
+(() => {
+  console.log("Connection Established");
+})();
 
 // Parentheses() UseCase: It converts the function into an expression so it can be executed immediately.
