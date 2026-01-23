816053aad1988b32a97eb247bc801ad04df0e780
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-23 06:09:18 +0500

    first commit

--- 9-Conditional_Statements/app.js
+++ 9-Conditional_Statements/app.js
@@ -25,24 +25,3 @@
 
 
 
-let num = 12;
-
-if ((num % 3 === 0) && ((num+1 == 15) || (num-1 == 11))) {
-    console.log("safe");
-}
-else{
-    console.log("unsafe");
-    
-}
-
-// Write a program to check if 2 numbers have the same last digit.
-// Eg : 32 and 47852 have the same last digit i.e. 2
-let n1 = 32;
-let n2 = 47852;
-
-if(n1 % 10 === n2 % 10){
-    console.log("Same digits");
-}
-else{
-    console.log("No, not a Same digits");
-}
