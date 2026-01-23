080b4b499b90361dd11eda8d7a79b32b1d04f9f0
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-01-23 06:07:15 +0500

    first commit

--- 9-Conditional_Statements/IfElse.js
+++ 9-Conditional_Statements/IfElse.js
@@ -1,4 +1,24 @@
+let myNum = 12;
 
+if ((myNum % 3 === 0) && ((myNum+1 == 15) || (myNum-1 == 11))) {
+    console.log("safe");
+}
+else{
+    console.log("unsafe");
+    
+}
+
+// Write a program to check if 2 numbers have the same last digit.
+// Eg : 32 and 47852 have the same last digit i.e. 2
+let n1 = 32;
+let n2 = 47852;
+
+if(n1 % 10 === n2 % 10){
+    console.log("Same digits");
+}
+else{
+    console.log("No, not a Same digits");
+}
 
 // 4. Popcorn prices Example:
 let size = 'xl';
