2a5c4d172860c8dbabcef1725426617a9004ec30
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-02-01 21:50:35 +0500

    first commit

--- 13-HighOrderFunction/Tasks.js
+++ 13-HighOrderFunction/Tasks.js
@@ -9,3 +9,13 @@ function filterNum(arr) {
     return arr.filter((value) => typeof value === 'number')
 }
 // console.log(filterNum(testArr));
+
+
+/* Task 1: Array Filtering
+Write a function filterNumbers(arr) that returns only numbers from a mixed array
+*/
+let someVal = [10, 20, 30]
+let revFunc = function (val) {
+    return val.reverse()
+}
+// console.log(revFunc(someVal));
