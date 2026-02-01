b0d5de90934fd6768e55a9e01563a83d253a93f1
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-02-01 22:00:07 +0500

    first commit

--- 13-HighOrderFunction/TasksHOF.js
+++ 13-HighOrderFunction/TasksHOF.js
@@ -5,3 +5,12 @@ function squareNumbers(num) {
     return num.map((value) => value * value)
 }
 // console.log(squareNumbers(numArr));
+
+
+
+// 2- Create a function filterEvenNumbers(arr) using filter() and arrow functions
+let secondArr = [1, 3, 6, 8, 2, 5]
+function filterEvenNumbers(val) {
+    return val.filter((value) => value %2 == 0)
+}
+// console.log(filterEvenNumbers(secondArr));
