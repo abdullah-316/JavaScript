6cdae46e71b699d174ecfd6852ee14c0fa8d3480
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-02-01 21:56:48 +0500

    first commit

--- 13-HighOrderFunction/Tasks.js
+++ 13-HighOrderFunction/Tasks.js
@@ -30,3 +30,15 @@ let maxNum = function (num) {
     return Math.max(...num)
 }
 // console.log(maxNum(myNum));
+
+
+/* Task 4: Remove Duplicates from an Array
+Write a function removeDuplicates(arr) that returns a new array with all duplicates removed
+*/
+// Rest Example
+function addEverything(...numbers) {
+    return numbers.reduce((a, b) => a + b);
+}
+// console.log(addEverything(1, 5, 10, 20));
+
+
