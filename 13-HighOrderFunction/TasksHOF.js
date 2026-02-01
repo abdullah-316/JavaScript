0707f53b4a3f37a33485d4d3002001127c1f7b3a
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-02-01 21:59:45 +0500

    first commit

---
+++ 13-HighOrderFunction/TasksHOF.js
@@ -0,0 +1,7 @@
+
+// 1- Write a function squareNumbers(arr) using map() and arrow functions
+let numArr = [4, 5, 6]
+function squareNumbers(num) {
+    return num.map((value) => value * value)
+}
+// console.log(squareNumbers(numArr));
