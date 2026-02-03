e3fff18738b1811c4bd030e8fc5563ca3b193c1f
Muhammad Abdullah <mabdullah00023@gmail.com>
2026-02-03 22:38:13 +0500

    first commit

---
+++ 14-DOM/Projects/1-ColorChanger/app.js
@@ -0,0 +1,34 @@
+
+const buttons = document.querySelectorAll(".button")
+const body = document.querySelector("body")
+
+buttons.forEach((button) => {
+    console.log(button);
+    button.addEventListener('click', function(e) {
+        // this mean which I need to target the element
+        console.log(e.target);
+        
+        switch (e.target.id) {
+            case "grey":
+                body.style.backgroundColor = e.target.id
+                break;
+                
+            case "white":
+                body.style.backgroundColor = e.target.id
+                break;
+
+            case "blue":
+                body.style.backgroundColor = e.target.id
+                break;
+
+            case "yellow":
+                body.style.backgroundColor = e.target.id
+                break;
+        
+            default:
+                break;
+        }
+        
+    })
+    
+})
